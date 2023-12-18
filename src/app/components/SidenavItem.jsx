22029642ad52ade0920aad221e57bcff2fe02ccb
Dave Goodman <david.goodman@unqork.com>
2023-12-18 13:44:32 -0500

    dummy data and addl component templates

---
+++ src/app/components/SidenavItem.jsx
@@ -0,0 +1,3 @@
+export default function SidenavItem() {
+  return "Sidenav Item";
+}
--- src/app/containers/FormContainer.jsx
+++ src/app/containers/FormContainer.jsx
@@ -1,6 +1,29 @@
 import Sidenav from "../components/Sidenav";
 import PersonalInfo from "../pages/PersonalInfo";
 
+const navSteps = [
+  {
+    id: 1,
+    stepLabel: "STEP 1",
+    stepDescription: "YOUR INFO",
+  },
+  {
+    id: 2,
+    stepLabel: "STEP 2",
+    stepDescription: "SELECT PLAN",
+  },
+  {
+    id: 3,
+    stepLabel: "STEP 3",
+    stepDescription: "ADD-ONS",
+  },
+  {
+    id: 4,
+    stepLabel: "STEP 4",
+    stepDescription: "SUMMARY",
+  },
+];
+
 export default function FormContainer() {
   return (
     <div>
