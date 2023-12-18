623650e04707afcc5c7ebc9cdfdf46f951a6bfba
Dave Goodman <david.goodman@unqork.com>
2023-12-18 13:38:40 -0500

    initial setup

---
+++ src/app/containers/FormContainer.jsx
@@ -0,0 +1,11 @@
+import Sidenav from "../components/Sidenav";
+import PersonalInfo from "../pages/PersonalInfo";
+
+export default function FormContainer() {
+  return (
+    <div>
+      <Sidenav />
+      <PersonalInfo />
+    </div>
+  );
+}
---
+++ src/app/pages/PersonalInfo.jsx
@@ -0,0 +1,3 @@
+export default function PersonalInfo() {
+  return "Personal Info Page";
+}
